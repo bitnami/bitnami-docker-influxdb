c960ecb368b501e06d5cfaa43037a0dcb16cb8df
Louis Cabrol <louis@cabrol.xyz>
2020-05-25 10:55:18 +0200

    fix: tcp.Mux error - add netcat package - remove wait-for-port causing the issue - replace with netcat for open port + curl ping endpoint for readiness

--- 1/debian-10/rootfs/opt/bitnami/scripts/libinfluxdb.sh
+++ 1/debian-10/rootfs/opt/bitnami/scripts/libinfluxdb.sh
@@ -266,7 +266,10 @@ influxdb_start_bg_noauth() {
     local start_command=("${INFLUXDB_BIN_DIR}/influxd" "-config" "$INFLUXDB_CONF_FILE")
     am_i_root && start_command=("gosu" "$INFLUXDB_DAEMON_USER" "${start_command[@]}")
     INFLUXDB_HTTP_HTTPS_ENABLED=false INFLUXDB_HTTP_BIND_ADDRESS="127.0.0.1:${INFLUXDB_HTTP_PORT_NUMBER}" debug_execute "${start_command[@]}" &
-    wait-for-port "$INFLUXDB_PORT_NUMBER"
+    while ! nc -zw1 127.0.0.1 $INFLUXDB_HTTP_PORT_NUMBER; do
+        sleep 1;
+    done;
+    curl -SL -I 127.0.0.1:$INFLUXDB_HTTP_PORT_NUMBER/ping?wait_for_leader=60s > /dev/null 2>&1
 }
 
 ########################
